5878f7051023c987bda9a1c50f0cabab429b2412
inserveofgod <inserveofgod@gmail.com>
2022-07-19 21:47:11 +0300

    category & selling sections are added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// initialize swiper
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 0,
+});
