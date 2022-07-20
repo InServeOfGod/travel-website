262eca70b3e5d8e4d97ca323741e0f684c8f4c03
inserveofgod <inserveofgod@gmail.com>
2022-07-20 21:36:47 +0300

    category, destinations, trip, brands, subscribe, footer sections are done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 
 // initialize swiper
 
@@ -6,6 +6,34 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 0,
-});
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+        960: {
+            spaceBetween: 25,
+            slidesPerView: 2,
+        },
+        1250: {
+           spaceBetween: 50,
+           slidesPerView: 3,
+        }
+    }
+})
+
+/*const swiper2 = new Swiper('.swiper-testimonials', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    //  pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})*/
